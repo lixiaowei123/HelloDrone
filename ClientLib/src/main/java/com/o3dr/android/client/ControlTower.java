c47cf069e4183db73ca789172ba5007568043f3d
Administrator <995439743@qq.com>
2020-09-27 11:36:32 +0800

    2020-9-27:上传HelloDrone Demo

---
+++ ClientLib/src/main/java/com/o3dr/android/client/ControlTower.java
@@ -0,0 +1,216 @@
+package com.o3dr.android.client;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.o3dr.android.client.interfaces.TowerListener;
+import com.o3dr.services.android.lib.drone.connection.ConnectionParameter;
+import com.o3dr.services.android.lib.model.IDroidPlannerServices;
+import com.o3dr.services.android.lib.model.IDroneApi;
+
+import org.droidplanner.services.android.impl.api.DroidPlannerService;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static android.content.Context.BIND_AUTO_CREATE;
+
+/**
+ * Created by fhuya on 11/12/14.
+ */
+public class ControlTower {
+
+    private static final String TAG = "LXW";
+
+    private final IBinder.DeathRecipient binderDeathRecipient = new IBinder.DeathRecipient() {
+        @Override
+        public void binderDied() {
+            notifyTowerDisconnected();
+        }
+    };
+    /**
+     * 创建连接服务
+     */
+    private final ServiceConnection o3drServicesConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            isServiceConnecting.set(false);
+            Log.i("lxw","EEE555");
+            o3drServices = IDroidPlannerServices.Stub.asInterface(service);
+            try {
+                o3drServices.asBinder().linkToDeath(binderDeathRecipient, 0);
+                Log.i("lxw","EEE333");
+                //通知连接
+                notifyTowerConnected();
+            } catch (RemoteException e) {
+                //通知没有连接
+                notifyTowerDisconnected();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.i("lxw","EEE777");
+            isServiceConnecting.set(false);
+            notifyTowerDisconnected();
+        }
+    };
+
+    private final AtomicBoolean isServiceConnecting = new AtomicBoolean(false);
+
+    private final Context context;
+    private final DroneApiListener apiListener;
+    private TowerListener towerListener;
+    private IDroidPlannerServices o3drServices;
+
+    public ControlTower(Context context) {
+        this.context = context;
+        this.apiListener = new DroneApiListener(this.context);
+    }
+
+    public boolean isTowerConnected() {
+        return o3drServices != null && o3drServices.asBinder().pingBinder();
+    }
+
+    /**
+     * 通知连接
+     */
+    void notifyTowerConnected()
+    {
+        Log.i("lxw","onTowerConnected");
+        if (towerListener == null)
+            return;
+        //调用连接
+        Log.i("lxw","onTower");
+        towerListener.onTowerConnected();
+    }
+
+    void notifyTowerDisconnected() {
+        if (towerListener == null)
+            return;
+
+        towerListener.onTowerDisconnected();
+    }
+
+    public Bundle[] getConnectedApps() {
+        Bundle[] connectedApps = new Bundle[0];
+        if (isTowerConnected()) {
+            try {
+                connectedApps = o3drServices.getConnectedApps(getApplicationId());
+                if (connectedApps != null) {
+                    final ClassLoader classLoader = ConnectionParameter.class.getClassLoader();
+                    for (Bundle appInfo : connectedApps) {
+                        appInfo.setClassLoader(classLoader);
+                    }
+                }
+            } catch (RemoteException e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
+        }
+
+        return connectedApps;
+    }
+
+    public void registerDrone(Drone drone, Handler handler) {
+        if (drone == null)
+            return;
+
+        if (!isTowerConnected())
+            throw new IllegalStateException("Control Tower must be connected.");
+
+        drone.init(this, handler);
+        drone.start();
+    }
+
+    public void unregisterDrone(Drone drone) {
+        if (drone != null)
+            drone.destroy();
+    }
+
+    /**
+     * 设定连接
+     * @param listener
+     */
+    public void connect(TowerListener listener,Context context) {
+        Log.i("LXW","AAA");
+        if (towerListener != null && (isServiceConnecting.get() || isTowerConnected()))
+
+            return;
+
+        if (listener == null) {
+
+            throw new IllegalArgumentException("ServiceListener argument cannot be null.");
+        }
+        Log.i("LXW","CCC");
+        towerListener = listener;
+        Log.i("LXW","DDD");
+        if (!isTowerConnected() && !isServiceConnecting.get()) {
+            final Intent serviceIntent = ApiAvailability.getInstance().getAvailableServicesInstance(context);
+            isServiceConnecting.set(context.bindService(serviceIntent, o3drServicesConnection,
+                    Context.BIND_AUTO_CREATE));
+        }
+        Log.i("LXW","EEE");
+    }
+//    public void connect(TowerListener listener) {
+//        boolean newValue;
+//
+//        Log.i("lxw","AAA");
+//        if (towerListener != null && (isServiceConnecting.get() || isTowerConnected()))
+//            return;
+//
+//        if (listener == null) {
+//            Log.i("lxw","BBB");
+//            throw new IllegalArgumentException("ServiceListener argument cannot be null.");
+//        }
+//
+//        towerListener = listener;
+//        Log.i("lxw","CCC");
+//        if (!isTowerConnected() && !isServiceConnecting.get())
+//        {
+//            Log.i("lxw","DDD");
+//
+//            Intent serviceIntent = ApiAvailability.getInstance().getAvailableServicesInstance(context);
+//
+//            newValue=context.bindService(serviceIntent, o3drServicesConnection, BIND_AUTO_CREATE);
+//            isServiceConnecting.set(newValue);
+//            Log.i("lxw","EEE");
+//        }
+//    }
+
+    public void disconnect() {
+        if (o3drServices != null) {
+            o3drServices.asBinder().unlinkToDeath(binderDeathRecipient, 0);
+            o3drServices = null;
+        }
+
+        notifyTowerDisconnected();
+
+        towerListener = null;
+
+        try {
+            //解绑连接
+            context.unbindService(o3drServicesConnection);
+        } catch (Exception e) {
+            Log.e(TAG, "Error occurred while unbinding from DroneKit-Android.");
+        }
+    }
+
+    IDroneApi registerDroneApi() throws RemoteException {
+        return o3drServices.registerDroneApi(this.apiListener, getApplicationId());
+    }
+
+    void releaseDroneApi(IDroneApi droneApi) throws RemoteException {
+        o3drServices.releaseDroneApi(droneApi);
+    }
+
+    private String getApplicationId() {
+        return context.getPackageName();
+    }
+}
---
+++ ClientLib/src/main/java/org/droidplanner/services/android/impl/communication/connection/usb/UsbConnection.java
@@ -0,0 +1,170 @@
+package org.droidplanner.services.android.impl.communication.connection.usb;
+
+import android.content.Context;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.o3dr.services.android.lib.gcs.link.LinkConnectionStatus;
+
+import org.droidplanner.services.android.impl.communication.connection.AndroidMavLinkConnection;
+import org.droidplanner.services.android.impl.core.MAVLink.connection.MavLinkConnectionTypes;
+import org.droidplanner.services.android.impl.core.model.Logger;
+import org.droidplanner.services.android.impl.utils.AndroidLogger;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class UsbConnection extends AndroidMavLinkConnection {
+
+    private static final String TAG = UsbConnection.class.getSimpleName();
+
+    private static final int FTDI_DEVICE_VENDOR_ID = 0x0403;
+
+    protected final int mBaudRate;
+
+    private UsbConnectionImpl mUsbConnection;
+
+    public UsbConnection(Context parentContext, int baudRate) {
+        super(parentContext);
+        mBaudRate = baudRate;
+    }
+
+    @Override
+    protected void closeConnection() throws IOException {
+        if (mUsbConnection != null) {
+            mUsbConnection.closeUsbConnection();
+        }
+    }
+
+    @Override
+    protected void loadPreferences() {
+    }
+
+    @Override
+    protected void openConnection(Bundle connectionExtras) throws IOException {
+        boolean id_device;
+        Log.i("lxw"," mUsbConnection:"+mUsbConnection);
+        if (mUsbConnection != null) {
+            try {
+                Log.i("lxw"," BBB123");
+                mUsbConnection.openUsbConnection(connectionExtras);
+                Log.d(TAG, "Reusing previous usb connection.");
+                return;
+            } catch (IOException e) {
+                Log.i("lxw"," BBB456");
+                Log.e(TAG, "Previous usb connection is not usable.", e);
+                mUsbConnection = null;
+            }
+        }
+        id_device=isFTDIdevice(context);
+        Log.i("lxw","id_device:"+id_device);
+        if (id_device)
+        {
+            final UsbConnectionImpl tmp = new UsbFTDIConnection(context, this, mBaudRate);
+            try {
+                tmp.openUsbConnection(connectionExtras);
+
+                // If the call above is successful, 'mUsbConnection' will be set.
+                mUsbConnection = tmp;
+                Log.d(TAG, "Using FTDI usb connection.");
+            } catch (IOException e) {
+                Log.d(TAG, "Unable to open a ftdi usb connection. Falling back to the open "
+                    + "usb-library.", e);
+            }
+        }
+
+        // Fallback
+        if (mUsbConnection == null) {
+            Log.i("lxw"," KKK");
+            Log.i("lxw"," BT:"+mBaudRate);
+            Log.i("lxw"," BT2:"+context);
+            final UsbConnectionImpl tmp = new UsbCDCConnection(context, this, mBaudRate);
+            Log.i("lxw"," BT3:"+tmp);
+            // If an error happens here, let it propagate up the call chain since this is the fallback.
+            tmp.openUsbConnection(connectionExtras);
+            Log.i("lxw"," BT5:");
+            mUsbConnection = tmp;
+            Log.i("lxw"," BT6:"+mUsbConnection);
+            Log.d(TAG, "Using open-source usb connection.");
+        }
+    }
+
+    private static boolean isFTDIdevice(Context context) {
+        UsbManager manager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
+        final HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
+        if (deviceList == null || deviceList.isEmpty()) {
+            return false;
+        }
+
+        for (Entry<String, UsbDevice> device : deviceList.entrySet()) {
+            if (device.getValue().getVendorId() == FTDI_DEVICE_VENDOR_ID) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected int readDataBlock(byte[] buffer) throws IOException {
+        if (mUsbConnection == null) {
+            throw new IOException("Uninitialized usb connection.");
+        }
+
+        return mUsbConnection.readDataBlock(buffer);
+    }
+
+    @Override
+    protected void sendBuffer(byte[] buffer) throws IOException {
+        if (mUsbConnection == null) {
+            throw new IOException("Uninitialized usb connection.");
+        }
+
+        mUsbConnection.sendBuffer(buffer);
+    }
+
+    @Override
+    public int getConnectionType() {
+        return MavLinkConnectionTypes.MAVLINK_CONNECTION_USB;
+    }
+
+    @Override
+    public String toString() {
+        if (mUsbConnection == null) {
+            return TAG;
+        }
+
+        return mUsbConnection.toString();
+    }
+
+    static abstract class UsbConnectionImpl {
+        protected final int mBaudRate;
+        protected final Context mContext;
+        private final UsbConnection parentConnection;
+        protected final Logger mLogger = AndroidLogger.getLogger();
+
+        protected UsbConnectionImpl(Context context, UsbConnection parentConn, int baudRate) {
+            mContext = context;
+            this.parentConnection = parentConn;
+            mBaudRate = baudRate;
+        }
+
+        protected void onUsbConnectionOpened(Bundle extras) {
+            parentConnection.onConnectionOpened(extras);
+        }
+
+        protected void onUsbConnectionStatus(LinkConnectionStatus connectionStatus) {
+            parentConnection.onConnectionStatus(connectionStatus);
+        }
+
+        protected abstract void closeUsbConnection() throws IOException;
+
+        protected abstract void openUsbConnection(Bundle extras) throws IOException;
+
+        protected abstract int readDataBlock(byte[] readData) throws IOException;
+
+        protected abstract void sendBuffer(byte[] buffer);
+    }
+}
